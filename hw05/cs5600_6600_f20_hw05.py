dd8a0d01d4afed2592c2476bf077acaa1afd0205
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-10-06 17:27:21 -0600

    added hw 5

---
+++ hw05/cs5600_6600_f20_hw05.py
@@ -0,0 +1,50 @@
+#/usr/bin/python
+
+###########################################
+# module: cs5600_6600_f20_hw05.py
+# YOUR NAME
+# YOUR A-NUMBER
+###########################################
+
+import numpy as np
+import tensorflow as tf
+import tflearn
+from tflearn.layers.core import input_data, fully_connected, dropout
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+import tflearn.datasets.mnist as mnist
+
+def make_tfl_convnet_slide22():
+    # your code here
+    pass
+
+def load_tfl_convnet_slide22(model_path):
+    # your code here
+    pass
+
+def make_shallow_tfl_ann():
+    # your code here
+    pass
+
+def make_deeper_tfl_convnet():
+    # your code here
+    pass
+
+def load_deeper_tfl_convnet(model_path):
+    # your code here
+    pass
+
+def load_shallow_tfl_ann(model_path):
+    # your code here
+    pass
+
+def fit_tfl_model(model, trainX, trainY, testX, testY, model_name, net_path, n_epoch=1, mbs=10):
+    # your code here
+    pass
+
+def test_tfl_model(model, X, Y):
+    # your code here
+    pass
+        
+    
+        
