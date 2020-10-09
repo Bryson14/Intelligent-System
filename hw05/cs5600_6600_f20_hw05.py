d8f06e773d592e160a6a4eb1008c99c85e46321c
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-10-09 17:45:20 -0600

    redo of function.

--- hw05/cs5600_6600_f20_hw05.py
+++ hw05/cs5600_6600_f20_hw05.py
@@ -35,32 +35,10 @@ def make_tfl_convnet_slide22():
     # X is the training data set; Y is the labels for X.
     # testX is the testing data set; testY is the labels for testX.
     # loads the data
-    X, Y, testX, testY = mnist.load_data(one_hot=True)
-    # shuffles the list knowing that each index in both list are correlated to the respective index of the other list
-    p = np.random.permutation(len(X))
-    X, Y = X[p], Y[p]
-    del p
-    p2 = np.random.permutation(len(testX))
-    testX, testY = testX[p2], testY[p2]
-    del p2
-
-    # splits X, Y into training and validation data.
-    trainX = X[0:50000]
-    trainY = Y[0:50000]
-    validX = X[50000:]
-    validY = Y[50000:]
-    trainX = trainX.reshape([-1, 28, 28, 1])
-    testX = testX.reshape([-1, 28, 28, 1])
-    validX = validX.reshape([-1, 28, 28, 1])
-
     model = define_convnet_slide22_architecture()
-    model.fit(trainX, trainY, n_epoch=1, shuffle=True, validation_set=(testX, testY), show_metric=True,
-              batch_size=10, run_id ="MNIST_ConvNet_1")
-    model.save("nets/ConvNet_Slide22.tfl")
     return model
 
 def load_tfl_convnet_slide22(model_path):
-    path = Path(model_path)
     model = define_convnet_slide22_architecture(load=True)
     model.load(model_path)
     return model
